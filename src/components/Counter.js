7531491d1f83ae362c8c4be262f375a600793c0b
chagansingh <chagansingh9876@gmail.com>
2023-02-19 07:31:20 +0530

    increment decrement added also by 5

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,35 @@
 import classes from './Counter.module.css';
+import { useSelector,useDispatch } from 'react-redux';
 
 const Counter = () => {
+  const dispatch=useDispatch()
+  const counter=useSelector(state=>state.counter);
+
+  const decrementHandler=()=>{
+    dispatch({type:'decrement'});
+  }
+  const incrementHandler=()=>{
+    dispatch({type:'increment'});
+  }
+  const decrementBy5Handler=()=>{
+    dispatch({type:'decrementBy5'});
+  }
+  const incrementBy5Handler=()=>{
+    dispatch({type:'incrementBy5'});
+  }
+
   const toggleCounterHandler = () => {};
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={decrementHandler}>Decrement</button>
+        <button onClick={incrementBy5Handler}>IncrementBy5</button>
+        <button onClick={decrementBy5Handler}>DecrementBy5</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
