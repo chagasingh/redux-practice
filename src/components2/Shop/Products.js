df60c195f221994c011fe5f1ffe6438b4e48db95
chagansingh <chagansingh9876@gmail.com>
2023-02-20 14:37:19 +0530

    redux done with cart and toggle

---
+++ src/components2/Shop/Products.js
@@ -0,0 +1,39 @@
+import ProductItem from './ProductItem';
+import classes from './Products.module.css';
+
+const DUMMY_PRODUCTS=[
+  {
+    id:'p1',
+    price:6,
+    title:'book',
+    description:'its good one'
+  },
+  {
+    id:'p2',
+    price:8,
+    title:'book2',
+    description:'its good one 2s'
+  },
+]
+
+const Products = (props) => {
+  return (
+    <section className={classes.products}>
+      <h2>Buy your favorite products</h2>
+      <ul>
+        {DUMMY_PRODUCTS.map((product)=>(
+          <ProductItem
+          key={product.id}
+          id={product.id}
+          title={product.title}
+          price={product.price}
+          description={product.description}
+        />
+        ))}
+        
+      </ul>
+    </section>
+  );
+};
+
+export default Products;
